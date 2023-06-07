0b6b3c879116082b471125699eeb6ab0a0af022b
Zina Artyugina <simfart@ya.ru>
2023-06-07 13:50:30 +0300

    tech list

--- src/components/Main/Techs/Techs.js
+++ src/components/Main/Techs/Techs.js
@@ -2,13 +2,24 @@ import React from 'react';
 
 function Techs() {
   return (
-    <section className="techs">
-    <h2 className="main__subtitle">Технологии</h2>
-    <div className="main__line main__line_tech "></div>
-    <h2 className='main__title main__title_tech'>7 технологий</h2>
-    <p className='techs__text'>На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.</p>
+    <section className='techs'>
+      <h2 className='main__subtitle'>Технологии</h2>
+      <div className='main__line main__line_tech '></div>
+      <h2 className='main__title main__title_tech'>7 технологий</h2>
+      <p className='techs__text'>На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.</p>
+<ul className='techs__list'>
+  <li className='techs__list__item'>HTML</li>
+  <li className='techs__list__item'>CSS</li>
+  <li className='techs__list__item'>JS</li>
+  <li className='techs__list__item'>React</li>
+  <li className='techs__list__item'>Git</li>
+  <li className='techs__list__item'>Express.js</li>
+  <li className='techs__list__item'>mongoDB</li>
+</ul>
+
+
     </section>
-  
+
   );
 }
 
